0a3abd72bd0aaa3107a30c394d9fcedc3c186848
ruthsundquist <46270298+ruthsundquist@users.noreply.github.com>
2019-03-02 14:22:16 -0600

    Add files via upload

---
+++ PyPoll/main.py
@@ -0,0 +1,42 @@
+import os
+import csv
+
+
+csvpath = r"C:\Users\HollyerRuthAnn\HW_Python\python-challenge\PyPoll\election_data.csv"
+
+with open(csvpath, newline ='' ) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+
+
+
+    VoteTallies ={"Khan": 0,"Correy": 0, "Li": 0, "O'Tooley": 0}
+
+    totalVotes = 0
+ 
+
+    for  row in csvreader:
+         totalVotes = totalVotes + 1
+         candidate = row[2]
+         VoteTallies[candidate]= VoteTallies[candidate] + 1
+         
+    for candidate in VoteTallies.keys(): 
+        "Khan"
+        Percentage = ' {:.3%}'.format (VoteTallies[candidate] / totalVotes)
+        VoteTallies[candidate] / totalVotes
+        votes = ("Percentage  " + str(Percentage))
+        print(candidate +':  '+ str(votes)) 
+
+        if VoteTallies[candidate]==max(VoteTallies.values()):
+            winner = candidate
+        
+
+print("Total Votes:  " + str(totalVotes))
+print("VoteTallies"+ str(VoteTallies))
+print("Winner:  " + winner)
+Poll = open("PyPoll.txt", "w")
+Poll.write("Total Votes:  " + str(totalVotes))
+Poll.write("VoteTallies"+ str(VoteTallies))
+Poll.write("Winner:  " + winner)
+Poll.close()
